8ef3d00476e70f2bea688d3229dec14d50a8f192
DocLJN <ljnoble@gmail.com>
2017-12-16 16:45:53 +0000

    SqlRunner class has single class method.

---
+++ db/sql_runner.rb
@@ -0,0 +1,23 @@
+# sql_runner.rb
+require('PG')
+
+class SqlRunner
+  # no initialize method needed as there is only a single class method
+
+  def self.run(sql, values=[])
+    # need the default argument to avoid having to provide values where none are needed
+    begin
+      db = PG.connect({dbname: 'inab', host: 'localhost'})
+      db.prepare('query', sql)
+      result = db.exec_prepared('query', values)
+    ensure
+      db.close() if db != nil
+    end
+    return result
+    end
+
+  end
+
+
+
+  # sql_runner.rb
