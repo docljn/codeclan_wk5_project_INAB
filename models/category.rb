49d3c12ace29436bff475b49a784b144433f7435
DocLJN <ljnoble@gmail.com>
2017-12-16 15:52:20 +0000

    Category class initialize method accepts name.

--- models/category.rb
+++ models/category.rb
@@ -1,7 +1,16 @@
 # category.rb
 require('pry-byebug')
 
+class Category
 
+  attr_reader :id
+  attr_accessor :name
+
+  def initialize(options)
+    @id = options['id'] if options['id']
+    @name = options['name']
+  end
+end
 
 
 
--- specs/category_spec.rb
+++ specs/category_spec.rb
@@ -5,5 +5,13 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 require_relative("../models/category.rb")
 
 class TestCategory < MiniTest::Test
-  
+
+  def setup
+    @food = Category.new({'name' => 'food'})
+  end
+
+  def test_category_has_name
+    assert_equal('food', @food.name)
+  end
+
 end
