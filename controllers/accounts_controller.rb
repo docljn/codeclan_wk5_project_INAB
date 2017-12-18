b780913d814517db49ddacecc745f71a0d627d7e
DocLJN <ljnoble@gmail.com>
2017-12-18 16:18:02 +0000

    Accounts edit details view.

--- controllers/accounts_controller.rb
+++ controllers/accounts_controller.rb
@@ -5,9 +5,29 @@ require("pry-byebug")
 
 require_relative("../models/account.rb")
 
-get("/accounts") do
+
+# read
+get "/accounts" do
   @accounts = Account.select_all()
   erb(:"/accounts/index")
+end
+
+# create
+get "/accounts/new"  do
+  erb(:"/accounts/new")
+end
+
+post "/accounts" do
+  account = Account.new(params)
+  account.save()
+  redirect to("/accounts")
+end
+
+# edit
+
+get "/accounts/:id/edit" do
+  @account = Account.select_one(params["id"])
+  erb(:"/accounts/edit")
 end
 
 
