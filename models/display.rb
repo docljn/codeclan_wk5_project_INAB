ebb485d8c062695da8b03bcb1e8c4fd712735904
DocLJN <ljnoble@gmail.com>
2017-12-19 10:38:54 +0000

    Transactions displays UK date format.

--- models/display.rb
+++ models/display.rb
@@ -13,6 +13,11 @@ class Display
     return "#{pounds}.#{pence}"
   end
 
-# Date::strptime(your_date_string,"%d-%m-%Y") to correctly parse a date string of format dd-mm-yyyy.
+  # Date::strptime(your_date_string,"%d-%m-%Y") to correctly parse a date string of format dd-mm-yyyy into a ruby date object
 
+  def self.date_dmy(date_string)
+    date_object = Date.strptime(date_string, "%Y-%m-%d")
+    uk_date = "#{date_object.day}/#{date_object.month}/#{date_object.year}"
+    return uk_date
+  end
 end
