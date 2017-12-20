b00f20097fbac1a70578664066a587c1a0aa2a05
DocLJN <ljnoble@gmail.com>
2017-12-20 10:27:59 +0000

    Refactor Display amount to use sprintf formatting.

--- models/display.rb
+++ models/display.rb
@@ -1,16 +1,23 @@
 class Display
 
+  # def self.amount(amount)
+  #   money_array = amount.to_s.split(".")
+  #   pounds = money_array[0]
+  #   pence = money_array[1]
+  #   if pence == nil
+  #     pence = "00"
+  #   end
+  #   if pence.length < 2
+  #     pence = pence + "0"
+  #   end
+  #   if pence.length > 2
+  #     pence = pence.round
+  #   end
+  #   return "#{pounds}.#{pence}"
+  # end
+
   def self.amount(amount)
-    money_array = amount.to_s.split(".")
-    pounds = money_array[0]
-    pence = money_array[1]
-    if pence == nil
-      pence = "00"
-    end
-    if pence.length < 2
-      pence = pence + "0"
-    end
-    return "#{pounds}.#{pence}"
+    sprintf('%.2f', amount)
   end
 
   # Date::strptime(your_date_string,"%d-%m-%Y") to correctly parse a date string of format dd-mm-yyyy into a ruby date object
