574f059403f0b704c3d970cd6c4c7ae57be8cd6d
DocLJN <ljnoble@gmail.com>
2018-01-06 10:34:29 +0000

    Fix formatting in sql_runner.rb

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -20,20 +20,20 @@ require('pg')
 #   end
 
 
-  class SqlRunner
+class SqlRunner
 
-    def self.run(sql, values=[])
-      begin
-        db = PG.connect({dbname: 'd9da040vu3o9u8', host: 'ec2-50-17-234-234.compute-1.amazonaws.com', port: 5432, user: 'ovybmxhzaootiw', password: 'dc52154a19030683506f544123b9cb510c9731ed10b490c24f8e6b525c8c5cc2' })
-        db.prepare('query', sql)
-        result = db.exec_prepared('query', values)
-      ensure
-        db.close() if db != nil
-      end
-      return result
-      end
-
+  def self.run(sql, values=[])
+    begin
+      db = PG.connect({dbname: 'd9da040vu3o9u8', host: 'ec2-50-17-234-234.compute-1.amazonaws.com', port: 5432, user: 'ovybmxhzaootiw', password: 'dc52154a19030683506f544123b9cb510c9731ed10b490c24f8e6b525c8c5cc2' })
+      db.prepare('query', sql)
+      result = db.exec_prepared('query', values)
+    ensure
+      db.close() if db != nil
     end
+    return result
+  end
+
+end
 
 
-  # sql_runner.rb
+# sql_runner.rb
